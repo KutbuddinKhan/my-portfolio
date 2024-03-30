fb8bc1c8ea92944b1ca31504109309c6bd25991f
Kutbuddin khan <Kutbuddinkhan82@gmail.com>
2024-03-30 10:36:01 +0530

    About me added

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,132 @@
+import { useEffect, useState } from "react";
+import Service from "./service";
+import Testimonial from "./testimonial";
+
+const services = [
+  {
+    title: "Web design",
+    icon: "/images/icon-design.svg",
+    description:
+      "The most modern and high-quality design made at a professional level.",
+  },
+  {
+    title: "Web development",
+    icon: "/images/icon-dev.svg",
+    description: "High-quality development of sites at the professional level.",
+  },
+  {
+    title: "Mobile apps",
+    icon: "/images/icon-app.svg",
+    description:
+      "Professional development of applications for iOS and Android.",
+  },
+  {
+    title: "Photography",
+    icon: "/images/icon-photo.svg",
+    description:
+      "I make high-quality photos of any category at a professional level.",
+  },
+];
+
 const About = () => {
-  return <div>About</div>;
+  const [testimonials, setTestimonials] = useState([]);
+
+  useEffect(() => {
+    fetch("testimonials.json")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data)
+        setTestimonials(data);
+      });
+  }, []);
+
+  return (
+    <div className="about active">
+      <header>
+        <h2 className="h2 article-title">About Me</h2>
+      </header>
+      <section className="about-text">
+        <p>
+          I am a dedicated and enthusiastic computer science student with a deep
+          passion for programming and a fascination for the potential of
+          technology to solve complex problems. I am committed to continuous
+          learning and stay updated with the latest trends in programming
+          languages, algorithms, and frameworks.
+        </p>
+        <p>
+          Currently, I am applying my knowledge in freelance projects for
+          StrawHatts Developers.
+        </p>
+      </section>
+
+      {/* services */}
+      <section className="service">
+        <h2 className="h3 service-title">What I&apos;m Doing</h2>
+        <ul className="service-list">
+          {services.map((service, index) => (
+            <Service
+              key={index}
+              title={service.title}
+              icon={service.icon}
+              description={service.description}
+            />
+          ))}
+        </ul>
+      </section>
+
+      {/* Testimonial section */}
+      <section className="testimonials">
+        <h3 className="h3  testimonials-title">Testimonials</h3>
+        <ul className="testimonials-list has-scrollbar">
+          {testimonials.map((testimonial, index) => (
+            <Testimonial
+              key={index}
+              name={testimonial.name}
+              avatar={testimonial.avatar}
+              testimonial={testimonial.testimonial}
+            />
+          ))}
+        </ul>
+      </section>
+
+      {/* clients logo */}
+      <section className="clients">
+        <h3 className="h3 clients-title">Clients</h3>
+        <ul className="clients-list has-scrollbar">
+          <li className="clients-item">
+            <a href="#">
+              <img src="/images/logo-1-color.png" alt="client logo" />
+            </a>
+          </li>
+          <li className="clients-item">
+            <a href="#">
+              <img src="/images/logo-2-color.png" alt="client logo" />
+            </a>
+          </li>
+          <li className="clients-item">
+            <a href="#">
+              <img src="/images/logo-3-color.png" alt="client logo" />
+            </a>
+          </li>
+          <li className="clients-item">
+            <a href="#">
+              <img src="/images/logo-4-color.png" alt="client logo" />
+            </a>
+          </li>
+          <li className="clients-item">
+            <a href="#">
+              <img src="/images/logo-5-color.png" alt="client logo" />
+            </a>
+          </li>
+          <li className="clients-item">
+            <a href="#">
+              <img src="/images/logo-6-color.png" alt="client logo" />
+            </a>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
 };
 
 export default About;
